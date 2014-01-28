caf7ffc15559b35fd611bdc5598c33a7a35824ed
Nexfer <ili4_region14@mail.ru>
2014-01-28 15:18:13 +0700

    Create mainwindow.h

---
+++ mainwindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+    
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    
+private:
+    Ui::MainWindow *ui;
+
+private slots:
+    void cezarFunc();
+    void vigenereFunc();
+    void steganoFunc();
+    void desFunc();
+};
+
+#endif // MAINWINDOW_H
